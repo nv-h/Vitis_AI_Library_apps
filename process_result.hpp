3ea91ef0c64c957c32833c26ef1cdc8219c7ce02
nv-h <saido.nv@gmail.com>
2020-07-03 20:32:57 -0700

    Display score and label

--- process_result.hpp
+++ process_result.hpp
@@ -14,40 +14,49 @@
  * limitations under the License.
  */
 #pragma once
+#include <iostream>
+#include <iomanip>
+
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+
+using namespace std;
+
 /*
  *   The color loops every 27 times,
  *    because each channel of RGB loop in sequence of "0, 127, 254"
  */
 static cv::Scalar getColor(int label) {
-  int c[3];
-  for (int i = 1, j = 0; i <= 9; i *= 3, j++) {
-    c[j] = ((label / i) % 3) * 127;
-  }
-  return cv::Scalar(c[2], c[1], c[0]);
+    int c[3];
+    for (int i = 1, j = 0; i <= 9; i *= 3, j++) {
+        c[j] = ((label / i) % 3) * 127;
+    }
+    return cv::Scalar(c[2], c[1], c[0]);
 }
 
 static cv::Mat process_result(cv::Mat &image,
                               const vitis::ai::YOLOv3Result &result,
                               bool is_jpeg) {
-  for (const auto bbox : result.bboxes) {
-    int label = bbox.label;
-    float xmin = bbox.x * image.cols + 1;
-    float ymin = bbox.y * image.rows + 1;
-    float xmax = xmin + bbox.width * image.cols;
-    float ymax = ymin + bbox.height * image.rows;
-    float confidence = bbox.score;
-    if (xmax > image.cols)
-      xmax = image.cols;
-    if (ymax > image.rows)
-      ymax = image.rows;
-    LOG_IF(INFO, is_jpeg) << "RESULT: " << label << "\t" << xmin << "\t" << ymin
-                          << "\t" << xmax << "\t" << ymax << "\t" << confidence
-                          << "\n";
-    cv::rectangle(image, cv::Point(xmin, ymin), cv::Point(xmax, ymax),
-                  getColor(label), 1, 1, 0);
-  }
-  return image;
+    for (const auto bbox : result.bboxes) {
+        int label = bbox.label;
+        auto color = getColor(label);
+        float xmin = bbox.x * image.cols + 1;
+        float ymin = bbox.y * image.rows + 1;
+        float xmax = xmin + bbox.width * image.cols;
+        float ymax = ymin + bbox.height * image.rows;
+        float confidence = bbox.score;
+        if (xmax > image.cols) xmax = image.cols;
+        if (ymax > image.rows) ymax = image.rows;
+
+        cv::rectangle(image, cv::Point(xmin, ymin), cv::Point(xmax, ymax),
+                      color, 1, 1, 0);
+        
+        stringstream ss;
+        ss << label << ": " << fixed << setprecision(2) << confidence << endl;
+        cv::putText(image, ss.str(), cv::Point(xmin, ymin-5),
+            cv::FONT_HERSHEY_SIMPLEX, 0.5, color, 1);
+    }
+
+    return image;
 }
